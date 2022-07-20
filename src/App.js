ba570a1835240bafa1aebbd4ac307e784548dd7f
Rosandymaulana <rosandy1118@gmail.com>
2022-07-20 21:44:27 +0700

    crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductList from './components/ProductList'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import AddProducts from './components/AddProducts'
+import EditProducts from './components/EditProducts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <ProductList />
+          </Route>
+          <Route path='/add'>
+            <AddProducts />
+          </Route>
+          <Route path='/edit/:id'>
+            <EditProducts />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
